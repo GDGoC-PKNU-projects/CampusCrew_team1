c4c306b581848b753a30359895f18ce9e5998ecb
naya1228 <jin100y43@gmail.com>
2026-04-30 12:48:22 +0900

    feat/auth-signup
    회원가입 API구현

--- backend/src/main/java/com/campuscrew/AuthService.java
+++ backend/src/main/java/com/campuscrew/AuthService.java
@@ -1,4 +1,39 @@
 package com.campuscrew;
 
+import com.campuscrew.dto.SignUpRequestDTO;
+import com.campuscrew.dto.SignUpResponseDTO;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class AuthService {
+    private final AuthRepository authRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public AuthService(AuthRepository authRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.authRepository = authRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public SignUpResponseDTO signUp(SignUpRequestDTO signUpRequest) {
+        if (authRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new IllegalArgumentException("Email already exists");
+        }
+
+        UserEntity userEntity = new UserEntity(
+                signUpRequest.getName(),
+                signUpRequest.getStudentId(),
+                signUpRequest.getEmail(),
+                passwordEncoder.encode(signUpRequest.getPassword())
+        );
+        UserEntity savedUser = authRepository.save(userEntity);
+
+        SignUpResponseDTO response = new SignUpResponseDTO();
+        response.setId(savedUser.getId());
+        response.setName(savedUser.getName());
+        response.setEmail(savedUser.getEmail());
+        return response;
+    }
+
 }
